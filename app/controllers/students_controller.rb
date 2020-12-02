0a05d3c236472669a784d66d8dc361b3be687f5c
ar00gula <arianna.mayer@gmail.com>
2020-12-02 12:30:38 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,6 +8,13 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def toggle_status
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
+
   private
 
     def set_student
